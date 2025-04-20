5fca2eec5ab4921dc0ddc3e032385da9a813bc38
BinuraGH <binuramenath617@gmail.com>
2025-04-20 15:24:30 +0530

    initial commit 1

--- backend/src/main/java/com/paf/backend/repository/LearningPlanRepository.java
+++
@@ -1,6 +0,0 @@
-package com.paf.backend.repository;
-
-public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
-    List<LearningPlan> findByUserId(String userId);
-
-}
